1724645fdb1fdc2e6addacde22173f5051d1a6b8
yunwisdom <qq695926920@163.com>
2020-09-02 10:29:38 +0800

    init

--- app/controller/mail.js
+++ app/controller/mail.js
@@ -13,15 +13,17 @@ class MailController extends Controller {
         const title = query.title || ctx.params.title;
         const description = query.description || ctx.params.description;
         const receiver = query.receiver || ctx.params.receiver;
+        const rurl = query.rurl || ctx.params.rurl;
+        const link = rurl ? `<div>链接：${rurl}</div>` : '';
 
-        console.log(title + ':' + description + ':' + receiver);
+        console.log(title + ':' + description + ':' + receiver + ':' + rurl);
 
         // sync
         const res = await app.mailer.send({
             to: receiver, // list of receivers
             subject: title, // Subject line
             text: description, // plain text body
-            html: `<b>${description}</b>`, // html body
+            html: `<b>${description}</b>${link}`, // html body
         });
 
         console.log(res.response);
