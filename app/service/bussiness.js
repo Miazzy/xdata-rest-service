16f25bf8bf11ab40c2942bff664c9c6218bf358c
yunwisdom <qq695926920@163.com>
2020-09-30 19:49:32 +0800

    init

--- app/service/bussiness.js
+++ app/service/bussiness.js
@@ -45,6 +45,45 @@ class BussinessService extends Service {
         // 设置返回结果
         return result;
     }
+
+    /**
+     * @function 获取用户管理组信息
+     * @param {*} username
+     */
+    async queryGroupLimitsByID(username) {
+
+        const { ctx, app } = this;
+
+        // 缓存控制器
+        const store = app.cache.store('redis');
+
+        // 获取动态token
+        const userinfo = await store.get(`wxConfig.enterprise.grouplimits.username#userid#@${username}`);
+
+        // 如果获取到缓存数据，设置返回结果
+        if (userinfo) {
+            return userinfo;
+        }
+
+        // 构建查询SQL
+        const sealmansql = ` select distinct seal from bs_seal_regist where seal is not null and seal != '' and seal like '%${username}%'  ; `;
+        const frontmansql = ` select distinct front from bs_seal_regist where front is not null and front != '' and front like '%${username}%'  ; `;
+        const archivemansql = ` select distinct archive from bs_seal_regist where archive is not null and archive != '' and archive like '%${username}%'  ; `;
+
+        // 查询印章人员
+        const seal = await app.mysql.query(sealmansql, []);
+        // 查询前端人员
+        const front = await app.mysql.query(frontmansql, []);
+        // 查询前端人员
+        const archive = await app.mysql.query(archivemansql, []);
+        // 设置结果
+        const result = { seal, front, archive };
+        // 设置缓存
+        await store.set(`wxConfig.enterprise.grouplimits.username#userid#@${username}`, result, 3600 * 24 * 3);
+
+        // 设置返回结果
+        return result;
+    }
 }
 
 
