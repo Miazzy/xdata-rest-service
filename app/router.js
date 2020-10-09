6253590f345c65cb2106059344f1f22fa23ec745
yunwisdom <qq695926920@163.com>
2020-10-09 12:53:16 +0800

    init

--- app/router.js
+++ app/router.js
@@ -46,6 +46,9 @@ module.exports = app => {
     // 查询员工数据
     router.get('/api/v2/queryemployee/:id', controller.database.queryEmployeeByID);
 
+    // 查询员工数据
+    router.get('/api/v2/employeewid/:id', controller.database.queryEmployeeByWID);
+
     // 查询文件信息，并进行下载
     router.get('/api/v1/filebase/:file/:path', controller.filebase.query);
 
