80c43cb79579526cf93312e609ebd4a19b88d3eb
yunwisdom <qq695926920@163.com>
2020-10-10 14:05:02 +0800

    init

--- app/service/bussiness.js
+++ app/service/bussiness.js
@@ -110,7 +110,7 @@ class BussinessService extends Service {
         // 缓存控制器
         const store = app.cache.store('redis');
 
-        const sql = `select id , dsporder wid , loginid username , lastname realname , sex , mobile , joblevel , textfield1 , certificatenum , status from newecology.dbo.hrmresource  where (status != 5) and  id = ${id} order by id asc offset 0 row fetch next 10000 row  only  `;
+        const sql = `select id , dsporder wid , loginid username , lastname realname , sex , mobile , joblevel level, textfield1 , certificatenum cert, status from newecology.dbo.hrmresource  where (status != 5) and  id = ${id} order by id asc offset 0 row fetch next 10000 row  only  `;
 
         const result = await this.pool.query(sql);
 
