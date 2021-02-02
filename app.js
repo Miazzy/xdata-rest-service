13e2fe83b8cf85b90121e30c47c7668db4b06e74
yunwisdom <qq695926920@163.com>
2021-02-02 11:21:52 +0800

    init

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-// 开始前执行
-app.beforeStart(async() => {
-    console.log('start');
-});
-// 准备好执行
-app.ready(async() => {
-    // 举例，获取数据库图片域名，放到缓存，便于使用
-    console.log('ready');
-});
-// 关闭前执行
-app.beforeClose(async() => {
-    console.log('close');
-});
+module.exports = app => {
+    // 开始前执行
+    app.beforeStart(async() => {
+        console.log('start');
+    });
+    // 准备好执行
+    app.ready(async() => {
+        console.log('ready');
+    });
+    // 关闭前执行
+    app.beforeClose(async() => {
+        console.log('close');
+    });
+};
